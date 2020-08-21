221e2eb9954fe01d641f0a3f766b6063fa0a60dd
ddubey239 <devang.dubey.civ16@itbhu.ac.in>
2020-08-21 13:45:19 +0530

    modified main and sumation

--- Sumation.java
+++ Sumation.java
@@ -1,6 +1,13 @@
 public class Sumation {
     public int sum(int a, int b){
+       return (a+b);
     }
     public int sum(int array[]){
+       int totalSum = 0;
+       int length = array.lenght;
+       for(int i=0; i<length; i++){
+          totalSum = sum(array[i], totalSum);
+       }
+       return totalSum;
     }
 }
