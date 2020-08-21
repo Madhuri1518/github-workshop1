0c17f7855212cf0fb66290e0837c5f4fb3fde3f4
Bonala Krishna <krishna.bonala@zemosolabs.com>
2020-08-21 11:59:16 +0530

    Sumation and main function completed

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 
 public class Main {
     public static void main(String args[]){
+	Sumation sm = new Sumation();
+	int [] array = new int [] { 1, 2, 3, 4, 5, 6};
+	int sum = sm.sum(array);
+	System.out.println("Sum of all elemets of the array is : "+sum);
     }
 }
