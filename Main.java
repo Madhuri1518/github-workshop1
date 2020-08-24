d8a46825d26ce9671df53bb5f56184780c7f2994
Bonala Krishna <krishna.bonala@zemosolabs.com>
2020-08-24 11:19:58 +0530

    updated main function

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 
 public class Main {
     public static void main(String args[]){
+	Sumation SumOfTwoNumbers = new Sumation();
+	int result = SumOfTwoNumbers.sum(12,13);
+	System.out.println("Sum of 12 and 13 is : " + result); 	
     }
 }
