16081285b05eb12e760944a64d8a5e40e9bb0374
ddubey239 <devang.dubey.civ16@itbhu.ac.in>
2020-08-24 11:19:23 +0530

    completed sum(int array[])

--- Sumation.java
+++ Sumation.java
@@ -3,5 +3,11 @@ public class Sumation {
 	return a+b;
     }
     public int sum(int array[]){
+        int totalSum = 0;
+        int length = array.lenght;
+        for(int i=0; i<length; i++){
+          totalSum = sum(array[i], totalSum);
+        }
+        return totalSum;
     }
 }
