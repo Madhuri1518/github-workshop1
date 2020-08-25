03423a89f4b680d13a9e7bb56c94ade1ad6cf81b
Devang Dubey <59962799+ddubey239@users.noreply.github.com>
2020-08-25 10:14:41 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,11 @@ public class Main {
     public static void main(String args[]){
 	Sumation SumOfTwoNumbers = new Sumation();
 	int result = SumOfTwoNumbers.sum(12,13);
-	System.out.println("Sum of 12 and 13 is : " + result); 	
+	System.out.println("Sum of 12 and 13 is : " + result);
+	
+	Sumation sumOfArray = new Sumation();
+        int [] intArray = new int [] {10, 20, 30, 40, 50};
+        int totalSum = sumOfArray.sum(intArray);
+        System.out.println("Sum of all elemets of the array is : "+ totalSum);
     }
 }
